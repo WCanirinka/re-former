4d40709ea8af1acf2a1c6b3d0b48d91266fd21be
Wilfried Canirinka <canirinkawilfried@gmail.com>
2019-12-18 00:32:28 +0200

    linters files added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
+  def edit
+    @user = User.find(params[:id])
+  end
 
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to '/users'
+    else
+      render 'edit'
     end
+  end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  private
 
-    def update
-      @user = User.find(params[:id])
-        if @user.update(user_params)
-          redirect_to  '/users'
-        else
-          render 'edit'
-        end
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :username, presence:true, length: { maximum: 50 }
-    validates :email, presence:true, length: { maximum: 255 }
-    validates :password, presence:true, length: { :within => 8..15 }
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { within: 8..15 }
 end
