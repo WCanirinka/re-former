d6fc6cb30a7450fdef25ad57711486cf24af6c6d
Wilfried Canirinka <canirinkawilfried@gmail.com>
2019-12-17 23:44:02 +0200

    Create the model user and the form

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, presence:true, length: { maximum: 50 }
+    validates :email, presence:true, length: { maximum: 255 }
+    validates :password, presence:true, length: { :within => 8..15 }
+end
