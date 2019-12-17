7a405cb3745a15cad51cc0e41b4a0cb137950588
300ms <ilhansnmz44@gmail.com>
2019-12-18 01:17:52 +0300

    Edit method and view are created.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,22 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+        if @user.update(user_params)
+          redirect_to  '/users'
+        else
+          render 'edit'
+        end
+    end
+
     private
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
-    end 
+    end
 end
